160f28a300c2f4a730a0859a5258eaf060f8b800
MrManoj124 <manojmanorooban@gmail.com>
2026-03-20 01:07:27 +0530

    create a children Array

--- fitness/lib/screens/home/home_screen.dart
+++ fitness/lib/screens/home/home_screen.dart
@@ -18,6 +18,7 @@ class HomeScreen extends StatelessWidget{
           physics: const BouncingScrollPhysics(),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
     )    
   }
 }
