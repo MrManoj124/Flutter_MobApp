9016e0a0314cf48984308015accdd1f0f8f9d689
MrManoj124 <manojmanorooban@gmail.com>
2026-03-24 23:56:10 +0530

    create ProfileScreen class

--- fitness/lib/screens/profile/profile_screen.dart
+++ fitness/lib/screens/profile/profile_screen.dart
@@ -2,3 +2,6 @@ import 'package:flutter/material.dart';
 import '../../theme/theme.dart';
 import '../../data/mock_data.dart';
 import '../../widgets/luxury_widgets.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
