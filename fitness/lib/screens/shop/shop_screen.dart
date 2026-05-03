7c5cd282ee18cdbba0614f017fb6a45b3942a687
MrManoj124 <manojmanorooban@gmail.com>
2026-05-03 14:50:13 +0530

    use override method to add createState method

--- fitness/lib/screens/shop/shop_screen.dart
+++ fitness/lib/screens/shop/shop_screen.dart
@@ -7,4 +7,6 @@ import 'package:flutter/material.dart';
 class ShopScreen extends StatefulWidget{
   const ShopScreen({super.key});
 
-  }
+  @override
+  State<ShopScreen> createState() => _ShopScreenState();
+}
