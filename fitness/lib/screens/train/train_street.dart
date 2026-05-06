02ec6e05dd711d2334654a40b993fc778d394d73
MrManoj124 <manojmanorooban@gmail.com>
2026-05-06 22:56:22 +0530

    build the widget with override

--- fitness/lib/screens/train/train_street.dart
+++ fitness/lib/screens/train/train_street.dart
@@ -6,3 +6,8 @@ import 'package:flutter/material.dart';
 
 class TrainScreen extends StatelessWidget {
   const TrainScreen({super.key});
+
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.black,
