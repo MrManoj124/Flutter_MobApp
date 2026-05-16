d86c0d59010e24fe2fdcb8d456c8f537be36e3c4
MrManoj124 <manojmanorooban@gmail.com>
2026-05-17 01:14:25 +0530

    create mockUser data

--- fitness/lib/data/mock_data.dart
+++ fitness/lib/data/mock_data.dart
@@ -2,3 +2,14 @@ import 'package : flutter/material.dart';
 import '../models/models.dart';
 import '../theme/app_colors.dart';
 
+// User 
+const mockUser = UserProfile(
+  name: 'Thanu Haran',
+  tier: 'Elite Member',
+  memberSince: '2022',
+  streakDays: 7,
+  totalSessions: 124,
+  totalKcal: 18240,
+  weeklyGoalPercent: 78,
+  todayKcal: 820,
+);
