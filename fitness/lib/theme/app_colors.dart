49ce9122539fe24fcaaa4ae5d8462653f87aaf20
MrManoj124 <manojmanorooban@gmail.com>
2026-06-10 23:56:55 +0530

    feat : the colors for Core Surfaces

--- fitness/lib/theme/app_colors.dart
+++ fitness/lib/theme/app_colors.dart
@@ -3,5 +3,10 @@ import 'package:flutter/material.dart';
 class AppColors {
   AppColors._();
 
+// ── Core surfaces ──────────────────────────────
+  static const Color black        = Color(0xFF080808);
+  static const Color surface      = Color(0xFF111111);
+  static const Color surface2     = Color(0xFF181818);
+  static const Color surface3     = Color(0xFF202020);
 
 }
