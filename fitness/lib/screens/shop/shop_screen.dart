6212687414884969e4209539d4b63e2c6eabcdc2
MrManoj124 <manojmanorooban@gmail.com>
2026-07-24 11:47:43 +0530

    feat : uncommand the imports

--- fitness/lib/screens/shop/shop_screen.dart
+++ fitness/lib/screens/shop/shop_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-// import '../../theme/theme.dart';
-// import '../../data/mock_data.dart';
-// import '../../models/models.dart';
-// import '../../widgets/luxury_widgets.dart';
+import '../../theme/theme.dart';
+import '../../data/mock_data.dart';
+import '../../models/models.dart';
+import '../../widgets/luxury_widgets.dart';
 
 class ShopScreen extends StatefulWidget{
   const ShopScreen({super.key});
