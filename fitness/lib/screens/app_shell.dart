1302c6c5b435f9b677516dc163da2c3a8fbe8ab8
MrManoj124 <manojmanorooban@gmail.com>
2026-07-24 11:38:21 +0530

    feat: correct import statement for TrainScreen in AppShell

--- fitness/lib/screens/app_shell.dart
+++ fitness/lib/screens/app_shell.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import '../theme/theme.dart';
 import 'home/home_screen.dart';
+import 'train/train_street.dart';
 import 'shop/shop_screen.dart';
-import 'train/train_screen.dart';
 import 'profile/profile_screen.dart';
 
 class AppShell extends StatefulWidget {
